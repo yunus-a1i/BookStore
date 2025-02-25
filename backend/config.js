f1db754074b7916ccd6ba35fd00fd1923650c9ee
Yunus Ali Siddiqui <yunusalisiddiqui90@gmail.com>
2025-02-25 09:42:42 +0530

    Update config.js

--- backend/config.js
+++ backend/config.js
@@ -1,6 +1,5 @@
-export const PORT = 5555;
+import dotenv from 'dotenv';
+dotenv.config();
 
-export const mongoDBURL =
- 'mongodb+srv://lmntrix260:9eY2sfM9B61AvM8r@bookstore.l7b4d.mongodb.net/myDatabase?retryWrites=true&w=majority&appName=BookStore';
-
-
+export const PORT = process.env.PORT;
+export const mongoDBURL = process.env.MONGO_URI;
